99e78a1a2395e2946ba974e828f3b7f3137555ca
ilhamsurya <ilhamsurya.m@gmail.com>
2021-09-23 18:41:48 +0700

    calculate length of line with two same points

---
+++ src/main/java/com/geometry/Line.java
@@ -0,0 +1,13 @@
+package com.geometry;
+
+public class Line {
+    private int x1, y1, x2, y2;
+
+    public Line(int x1, int y1, int x2, int y2) {
+
+    }
+
+    public double length() {
+        return 0;
+    }
+}
---
+++ src/test/java/com/geometry/LineTest.java
@@ -0,0 +1,15 @@
+package com.geometry;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LineTest {
+    private static final double EPS = 0.0001;
+
+    @Test
+    public void shouldReturnZeroWhenPointsAreIdentical() {
+        Line line = new Line(0, 0, 0, 0);
+        assertEquals(0, line.length(), EPS);
+    }
+}
